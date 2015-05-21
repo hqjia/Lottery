c3fc21f9d7bd0e3bc0a4292b919c6a436e210c26
hqjia <jiahongqiang@gmail.com>
2015-05-21 23:16:41 +0800

    抽奖
    可以抽奖的c#小程序

---
+++ Lottery/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            t.Abort();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("宋体", 48F);
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(233, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 250);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("宋体", 48F);
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(483, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 250);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("宋体", 48F);
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(733, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 250);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("宋体", 48F);
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(733, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 250);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("宋体", 48F);
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(733, 560);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(250, 250);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("宋体", 48F);
+            this.button6.ForeColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(483, 560);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(250, 250);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("宋体", 48F);
+            this.button7.ForeColor = System.Drawing.Color.Red;
+            this.button7.Location = new System.Drawing.Point(233, 560);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(250, 250);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("宋体", 48F);
+            this.button8.ForeColor = System.Drawing.Color.Red;
+            this.button8.Location = new System.Drawing.Point(233, 310);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(250, 250);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.Font = new System.Drawing.Font("宋体", 48F);
+            this.buttonStart.ForeColor = System.Drawing.Color.Red;
+            this.buttonStart.Location = new System.Drawing.Point(483, 310);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(250, 250);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "button9";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1236, 885);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "祝你好运！";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Lottery/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Lottery
+{
+    public partial class Form1 : Form
+    {
+        static int STATUS;
+        static int FLAG = 0;
+        static Thread t;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+            
+            t = new Thread(listen);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+
+        static void listen()
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 26379);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sock.Bind(ipep);
+            //sock.Listen(3);
+            while(true){
+                try {
+                    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                    EndPoint Remote = (EndPoint)(sender);
+                byte[] bytes = new byte[32];
+                int bytenum = sock.ReceiveFrom(bytes, ref Remote);
+                //MessageBox.Show(bytenum.ToString() + " receivd.");
+                FLAG = bytes[0];
+                sock.SendTo(bytes, bytenum, SocketFlags.None, Remote);
+                
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void init(){
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            STATUS = 0;
+            buttonStart.Text = "开始";
+            button1.Text = "";
+            button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            button2.Text = "";
+            button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            button3.Text = "";
+            button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            button4.Text = "";
+            button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            button5.Text = "";
+            button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            button6.Text = "";
+            button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            button7.Text = "";
+            button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            button8.Text = "";
+            button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+        }
+
+        private void buttonPrint(int i, string text)
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1.BackgroundImage = null;
+            button2.BackgroundImage = null;
+            button3.BackgroundImage = null;
+            button4.BackgroundImage = null;
+            button5.BackgroundImage = null;
+            button6.BackgroundImage = null;
+            button7.BackgroundImage = null;
+            button8.BackgroundImage = null;
+
+            switch (i)
+            {
+                case 1:
+                    button1.Text = text;
+                    button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+                    break;
+                case 2:
+                    button2.Text = text;
+                    button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+                    break;
+                case 3:
+                    button3.Text = text;
+                    button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+                    break;
+                case 4:
+                    button4.Text = text;
+                    button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+                    break;
+                case 5:
+                    button5.Text = text;
+                    button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+                    break;
+                case 6:
+                    button6.Text = text;
+                    button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+                    break;
+                case 7:
+                    button7.Text = text;
+                    button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+                    break;
+                case 8:
+                    button8.Text = text;
+                    button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void buttonFlash(int i, int time)
+        {
+            switch (i)
+            {
+                case 1:
+                    button1.FlatStyle = FlatStyle.Standard;
+                    button1.Update();
+                    Thread.Sleep(time);
+                    button1.FlatStyle = FlatStyle.Flat;
+                    button1.Update();
+                    break;
+                case 2:
+                    button2.FlatStyle = FlatStyle.Standard;
+                    button2.Update();
+                    Thread.Sleep(time);
+                    button2.FlatStyle = FlatStyle.Flat;
+                    button2.Update();
+                    break;
+                case 3:
+                    button3.FlatStyle = FlatStyle.Standard;
+                    button3.Update();
+                    Thread.Sleep(time);
+                    button3.FlatStyle = FlatStyle.Flat;
+                    button3.Update();
+                    break;
+                case 4:
+                    button4.FlatStyle = FlatStyle.Standard;
+                    button4.Update();
+                    Thread.Sleep(time);
+                    button4.FlatStyle = FlatStyle.Flat;
+                    button4.Update();
+                    break;
+                case 5:
+                    button5.FlatStyle = FlatStyle.Standard;
+                    button5.Update();
+                    Thread.Sleep(time);
+                    button5.FlatStyle = FlatStyle.Flat;
+                    button5.Update();
+                    break;
+                case 6:
+                    button6.FlatStyle = FlatStyle.Standard;
+                    button6.Update();
+                    Thread.Sleep(time);
+                    button6.FlatStyle = FlatStyle.Flat;
+                    button6.Update();
+                    break;
+                case 7:
+                    button7.FlatStyle = FlatStyle.Standard;
+                    button7.Update();
+                    Thread.Sleep(time);
+                    button7.FlatStyle = FlatStyle.Flat;
+                    button7.Update();
+                    break;
+                case 8:
+                    button8.FlatStyle = FlatStyle.Standard;
+                    button8.Update();
+                    Thread.Sleep(time);
+                    button8.FlatStyle = FlatStyle.Flat;
+                    button8.Update();
+                    break;
+                default:
+                    break;
+            }
+                 
+        }
+
+        private void randomlot()
+        {
+            Random rd = new Random();
+
+            int n = rd.Next(1, 9);
+
+            for (int i = 0; i < 24 + n; i++)
+            {
+                     int num = i % 8 + 1;
+                     int time = 10 * ((i / 2 )+ 1);
+                     buttonFlash(num, time);
+            }
+            if (n == 1 || n == 6)
+            {
+                buttonPrint(n, "");
+            }
+            else
+            {
+                buttonPrint(n, "");
+            }
+            
+        }
+
+        private void buzhong(){
+            Random rd = new Random();
+
+            int n = rd.Next(1, 9);
+            if (n == 1 || n == 6){
+                n++;
+            }
+
+            for (int i = 0; i < 24 + n; i++)
+            {
+                     int num = i % 8 + 1;
+                     int time = 10 * ((i / 2 )+ 1);
+                     buttonFlash(num, time);
+            }
+            if (n == 1 || n == 6)
+            {
+                buttonPrint(n, "");
+            }
+            else
+            {
+                buttonPrint(n, "");
+            }
+        }
+
+        private void bizhong()
+        {
+            Random rd = new Random();
+
+            int n = rd.Next(1, 3);
+            if (n > 1)
+            {
+                n = 6;
+            }
+
+            for (int i = 0; i < 24 + n; i++)
+            {
+                int num = i % 8 + 1;
+                int time = 10 * ((i / 2) + 1);
+                buttonFlash(num, time);
+            }
+            if (n == 1 || n == 6)
+            {
+                buttonPrint(n, "");
+            }
+            else
+            {
+                buttonPrint(n, "");
+            }
+        }
+
+        private void doLottery()
+        {
+            switch (FLAG)
+            {
+                case 0:
+                    randomlot();
+                    break;
+                case 1:
+                    bizhong();
+                    break;
+                case 2:
+                    buzhong();
+                    break;
+                default:
+                    return;
+                      
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (STATUS == 0)
+            {
+                doLottery();
+                STATUS = 1;
+                buttonStart.Text = "再来";
+            }
+            else
+            {
+                init();
+            }
+        }
+
+    }
+}
